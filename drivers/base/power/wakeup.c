42de59e0f5abaa32b9296f6c3f98bb4e4be151c1
Francisco Franco <franciscofranco.1990@gmail.com>
2016-08-31 17:37:43 +0000

    drivers: wakeup: add toggles for wlan_wow_wl and wlan wakelocks
    As usual users are responsible for any issue that might occur
    by disabling these wakelocks. This merely gives userspace the
    power to disable these if they are creating havoc by invoking
    the Burning Legion. For the horde!
    
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -22,6 +22,10 @@
 
 static bool enable_bluedroid_timer_ws = true;
 module_param(enable_bluedroid_timer_ws, bool, 0644);
+static bool enable_wlan_wow_wl_ws = true;
+module_param(enable_wlan_wow_wl_ws, bool, 0644);
+static bool enable_wlan_ws = true;
+module_param(enable_wlan_ws, bool, 0644);
 
 /*
  * If set, the suspend/hibernate code will abort transitions to a sleep state
@@ -416,7 +420,9 @@ static void wakeup_source_activate(struct wakeup_source *ws)
 {
 	unsigned int cec;
 
-	if (!enable_bluedroid_timer_ws && !strcmp(ws->name, "bluedroid_timer"))
+	if ((!enable_bluedroid_timer_ws && !strcmp(ws->name, "bluedroid_timer")) ||
+		(!enable_wlan_wow_wl_ws && !strcmp(ws->name, "wlan_wow_wl")) ||
+		(!enable_wlan_ws && !strcmp(ws->name, "wlan")))
 		return;
 
 	/*
